edcdd69e89e6bd2c6e25367d29e069d2aac7102c
lawrizy <lawrizy@gmail.com>
2022-05-10 19:15:51 +0200

    company model

--- ASSETS/models.py
+++ ASSETS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=255, unique=True, null=False)
+    company_description = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.company_name
