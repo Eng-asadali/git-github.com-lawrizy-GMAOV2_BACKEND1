ed45b2c0c3d31ff2ea4eaef1416b43e4c4ea8362
lawrizy <lawrizy@gmail.com>
2022-06-14 19:39:04 +0200

    add id in the serializer

--- ASSETS/serializers.py
+++ ASSETS/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)  # to force the serializer to show the id
+
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = ['id', 'company_name', 'company_description','url']#'__all__'
