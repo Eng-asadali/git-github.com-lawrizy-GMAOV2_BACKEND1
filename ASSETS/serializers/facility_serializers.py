808ecade5f048ffb51b416c4114503756f500c15
lawrizy <lawrizy@gmail.com>
2022-09-13 16:01:40 +0200

    update facility v1

--- ASSETS/serializers/facility_serializers.py
+++ ASSETS/serializers/facility_serializers.py
@@ -3,6 +3,12 @@ from ..models.facility_models import *
 
 
 class FacilitySerializer(serializers.HyperlinkedModelSerializer):
+    # we have to force the serializers to allow null and blank even if defined in model
+    city = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+    locality = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+    number = serializers.IntegerField(allow_null=True, required=False)
+    postal_code = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+    image = serializers.ImageField(allow_null=True, allow_empty_file=True, required=False)
 
     class Meta:
         model = Facility
--- ASSETS/views/facility_views.py
+++ ASSETS/views/facility_views.py
@@ -1,9 +1,25 @@
 from rest_framework import viewsets
 from ..models import Facility
 from ..serializers import FacilitySerializer
+from rest_framework.response import Response
 
 
 # viewsets are easier than apiview because we don't have to define the methods
 class FacilityViewset(viewsets.ModelViewSet):
     queryset = Facility.objects.all()
     serializer_class = FacilitySerializer
+
+    def update(self, request, *args, **kwargs):
+        print("AZIZ request.data: ",request.data)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
