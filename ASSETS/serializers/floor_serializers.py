71595e055ad69d496bd000f4755f8a2a318803f4
lawrizy <lawrizy@gmail.com>
2022-09-23 13:44:11 +0200

    update floor serializer to hyperlinked

--- ASSETS/serializers/floor_serializers.py
+++ ASSETS/serializers/floor_serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from ..models import FloorModel
 
 
-class FloorSerializer(serializers.ModelSerializer):
+class FloorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FloorModel
         fields = '__all__'
