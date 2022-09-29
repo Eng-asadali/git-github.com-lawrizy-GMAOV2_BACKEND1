46f26aa16cbb393eac73b832276fcdff14049284
lawrizy <lawrizy@gmail.com>
2022-09-29 19:29:30 +0200

    rename eqp field

--- ASSETS/models/equipment_models.py
+++ ASSETS/models/equipment_models.py
@@ -15,8 +15,8 @@ class EquipmentModel(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=250, null=True, blank=True)
     parent_id = models.ForeignKey('self', on_delete=models.PROTECT, null=True, related_name='children', blank=True)
-    room = models.ForeignKey(RoomModel, on_delete=models.PROTECT, null=True, related_name='equipment')
-    family = models.ForeignKey(EquipmentFamilyModel, on_delete=models.PROTECT, null=True, related_name='family_children')
+    room_id = models.ForeignKey(RoomModel, on_delete=models.PROTECT, null=True, related_name='equipment')
+    family_id = models.ForeignKey(EquipmentFamilyModel, on_delete=models.PROTECT, null=True, related_name='family_children')
 
     def __str__(self):
         return self.name
