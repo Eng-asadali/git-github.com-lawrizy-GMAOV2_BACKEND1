0a6d24658737a3e6246b93a749be26ef09f23f3d
lawrizy <lawrizy@gmail.com>
2022-10-04 13:54:18 +0200

    update url

--- WORKS/urls.py
+++ WORKS/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .views import DomainViewset, JobTypeViewset
+from .views import DomainViewset, JobTypeViewset, JobViewset
 
 router = routers.SimpleRouter()
 router.register(r'domain', DomainViewset)
 router.register(r'job_type', JobTypeViewset)
+router.register(r'job',JobViewset)
 
 urlpatterns = router.urls
