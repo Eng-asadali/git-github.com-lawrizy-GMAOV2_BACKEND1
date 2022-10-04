0252bb86c747788ddb751cdc29177faea8a1bc08
lawrizy <lawrizy@gmail.com>
2022-10-04 15:20:56 +0200

    unique constraint for jobs

--- WORKS/models/job_models.py
+++ WORKS/models/job_models.py
@@ -5,7 +5,7 @@ class JobTypeModel(models.Model):
     """
     Job Type est utilisé pour le type d'intervention CORRECTIF - PREVENTIF - SEC
     """
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=200)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class JobModel(models.Model):
     """
     Job est pour l'intervention, exemple: entretien mensuel
     """
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
     job_type_id = models.ForeignKey(JobTypeModel, on_delete=models.PROTECT, null=False, related_name='job_type')
     domain_id = models.ForeignKey(DomainModel, on_delete=models.PROTECT, null=False, related_name='domain')
