f699f0973162f3731ec4ee989841aafad20887f1
lawrizy <lawrizy@gmail.com>
2022-10-12 18:47:20 +0200

    create user method

--- HR/serializer.py
+++ HR/serializer.py
@@ -7,6 +7,14 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = '__all__'
 
+    # la methode create() est utilisée car le password doit être hasché
+    def create(self, validated_data):
+        print("Aziz validated_data",validated_data)
+        user = User.objects.create_user(username=validated_data["username"],password=validated_data["password"])
+        user.save()
+        return user
+
+
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     username = serializers.CharField(source='user.username')
     email = serializers.CharField(source='user.email')
