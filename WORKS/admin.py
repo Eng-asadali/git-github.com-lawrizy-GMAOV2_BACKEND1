e7d3416495be5b4bccee99feac51c5975f86fd3a
lawrizy <lawrizy@gmail.com>
2022-10-16 22:17:06 +0200

    add WorkOrderModel to admin

--- WORKS/admin.py
+++ WORKS/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import DomainModel, JobTypeModel, JobModel
+from .models import DomainModel, JobTypeModel, JobModel,WorkOrderModel
 
 # Register your models here.
 admin.site.register(DomainModel)
 admin.site.register(JobTypeModel)
 admin.site.register(JobModel)
-
+admin.site.register(WorkOrderModel)
 
