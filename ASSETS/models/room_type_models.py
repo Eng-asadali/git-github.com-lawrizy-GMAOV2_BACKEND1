5694c30f76f0dfb99af291177fc562b3901d954e
lawrizy <lawrizy@gmail.com>
2022-12-02 15:18:58 +0100

    room_type upload

--- ASSETS/models/room_type_models.py
+++ ASSETS/models/room_type_models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class RoomTypeModel(models.Model):
-    room_type = models.CharField(max_length=40, null=False, blank=False)
-    code = models.CharField(max_length=2, null=False, blank=False)
+    room_type = models.CharField(max_length=40, null=False, blank=False, unique=True)
+    code = models.CharField(max_length=2, null=False, blank=False, unique=False)  # attention on le code n'est pas unique
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['room_type']),
+        ]
 
     def __str__(self):
         value = (self.room_type+" -- "+self.code)
--- ASSETS/views/room_type_views.py
+++ ASSETS/views/room_type_views.py
@@ -1,8 +1,42 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from ..serializers import RoomTypeSerializer
 from ..models import RoomTypeModel
+import csv
 
 
 class RoomTypeViewset(viewsets.ModelViewSet):
     queryset = RoomTypeModel.objects.all()
     serializer_class = RoomTypeSerializer
+    authentication_classes = [TokenAuthentication]  # to use token authentication
+    permission_classes = [IsAuthenticated]  # to force authentication
+    ordering_fields = ['room_type']
+
+
+#cette classe est utilisé pour import du fichier csv
+class RoomTypeUploadView(APIView):
+    parser_classes = [FormParser, MultiPartParser]
+    authentication_classes = [TokenAuthentication]  # to use token authentication
+    permission_classes = [IsAuthenticated]  # to force authentication
+
+    def post(self, request):
+        contenu = request.data['fichier']    # we get the file from the request
+        contenu = contenu.read().decode("utf8").split('\n')     # for csv.reader(), we need to split a string by line
+        lines = csv.reader(contenu, delimiter=',')
+        print("AZIZ lines in file: ",lines)
+        for line, i in zip(lines, range(len(contenu))):  # zip maps the 2 vectors per item -- i is used as counter
+            if (i == 0) and ((line[0] != "room_type") or (line[1] != "code")):
+                print("Aziz proble upload: ", line)
+                content = {'upload company file': 'bad csv file structure'}
+                return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)  # it means that the file is not good
+            elif i != 0:  # exclude the header of the csv file
+                a_room_type = RoomTypeModel(room_type=line[0], code=line[1])
+                a_room_type.save()
+                print("AZIZ upload done: ",line)
+        content = {'upload company file': 'received and created'}
+        return Response(content, status=status.HTTP_200_OK)
