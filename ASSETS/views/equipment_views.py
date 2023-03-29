2908a9e4f6e48af1405b0f96f2a4fbcb62ae0019
lawrizy <lawrizy@gmail.com>
2023-03-29 13:35:44 +0200

    _Add filter in EQP

--- ASSETS/views/equipment_views.py
+++ ASSETS/views/equipment_views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 from ..serializers import EquipmentFamilySerializer, EquipmentSerializer
 from ..models import EquipmentModel, EquipmentFamilyModel
+from django_filters.rest_framework import DjangoFilterBackend # to filter the queryset
+from rest_framework import filters # to filter the queryset
 
 
 class EquipmentViewset(viewsets.ModelViewSet):
     serializer_class = EquipmentSerializer
     queryset = EquipmentModel.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]  # to filter the queryset
+    filterset_fields = ['room_id']  # to filter by facility name or facility id
 
 
 class EquipmentFamilyViewset(viewsets.ModelViewSet):
